9d0ffd321659dc8a8e949f20bf84e3f7ebd4cde5
symbol <symbol_for_success@hotmail.com>
2016-08-13 21:13:35 +1200

    table loads from json file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,40 +11,77 @@
 angular.module('practicalAssignmentApp')
   .controller('MainCtrl', function() {
 
-  }).controller('loadPlayersController', function($scope, $http) {
+  });
+
+/**
+  angular.module('practicalAssignmentApp').controller('loadPlayersController', function($scope, $http) {
     $scope.testValue = "this is a test";
+
     //test for data bind
-    $scope.playerData = gems;
+    //$scope.playerData = gems;
 
-    $http.get("data.json")
-      .then(function(response) {});
-  }).controller('addPlayerController', function($scope) {
+    $http.get('data.json')
+      .success(function(res) {
+        $scope.playerData = res;
+      });
+
+  });
+  **/
+
+angular.module('practicalAssignmentApp').controller('loadPlayersController', ['$scope', '$http', function($scope, $http) {
+  //  $scope.testValue = "this is a test";
+  //test for data bind
+  //$scope.playerData = gems;
 
 
+
+  $http.get('data/data.json').success(function(data) {
+    $scope.playerData = data;
   });
 
 
+  /*test for json file
+  $http.get('http://mysafeinfo.com/api/data?list=englishmonarchs&format=json').success(function(data) {
+    $scope.playerData = data;
+  });
+  */
+
+}]);
+
+/*
+  .controller('addPlayerController', function($scope) {
 
 
-   var gems = [{
-     firstName: "andy",
-     Surname: "crowe",
-     Sex: "images/icon-male.png",
-     Tier: "images/tier_gold.png",
-     Email: "1@andrewcrowe.nz"
+  });
+*/
 
-   }, {
-     firstName: "andy",
-     Surname: "crowe",
-     Sex: "images/icon-male.png",
-     Tier: "images/tier_gold.png",
-     Email: "1@andrewcrowe.nz"
 
-   }, {
-     firstName: "andy",
-     Surname: "crowe",
-     Sex: "images/icon-male.png",
-     Tier: "images/tier_gold.png",
-     Email: "1@andrewcrowe.nz"
+/* test data
+var gems = [{
+  firstName: "andy",
+  Surname: "crowe",
+  Sex: "images/icon-male.png",
+  Tier: "images/tier_gold.png",
+  Email: "1@andrewcrowe.nz"
 
-   }];
+}, {
+  firstName: "andy",
+  Surname: "crowe",
+  Sex: "images/icon-male.png",
+  Tier: "images/tier_gold.png",
+  Email: "1@andrewcrowe.nz"
+
+}, {
+  firstName: "andy",
+  Surname: "crowe",
+  Sex: "images/icon-male.png",
+  Tier: "images/tier_gold.png",
+  Email: "1@andrewcrowe.nz"
+
+}];
+*/
+
+
+/* test angular code to load local file
+var json = require('data.json');
+*/
